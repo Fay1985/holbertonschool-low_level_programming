863f2825d9ecc0608b392e24dc7336fd48fa392e
Fay1985 <faycal.hadji@live.fr>
2025-02-17 16:15:08 +0000

    fourth file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main(void) // This function main prints a message to the console
+int main(void) /* This function main prints a message to the console */
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
