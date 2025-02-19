158d994598a8c2bed338e1b8e9b5349d60e04ff4
Fay1985 <faycal.hadji@live.fr>
2025-02-19 14:56:55 +0000

    update7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * main function prints the full alphabet and a new line.
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
