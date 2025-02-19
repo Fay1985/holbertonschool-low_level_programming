3fd708161c91413b5b8c88b6a58717c5209a9b2e
Fay1985 <faycal.hadji@live.fr>
2025-02-19 12:59:45 +0000

    update3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * main function prints the full alphabet and a new line.
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+    char c;
+    for (c = 'a'; c <= 'z'; c++)
+    {
+        if (c == 'q' || c == 'e')
+            continue;
+        putchar(c);
+    }
+    putchar('\n');
+    return (0);
+}
