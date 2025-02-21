597fe4a3653edc62b6d926cb2d3ad7494427aef8
Fay1985 <faycal.hadji@live.fr>
2025-02-21 11:27:18 +0000

    hello

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry poin
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	for(i = 97; i < 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+        return (0);
+}
+
---
+++ functions_nested_loops/1-main.h
@@ -0,0 +1,5 @@
+#ifndef
+#define 1-MAIN_H
+void print_alphabet(void);
+
+#endif
