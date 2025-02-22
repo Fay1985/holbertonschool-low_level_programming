60eda1ee92821be1eb7af2d2b8b2c69279d68e04
Fay1985 <faycal.hadji@live.fr>
2025-02-22 11:20:32 +0000

    update4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- *
- *isalpha - function checks if alpha or number
- *
- *
- */
+* _isalpha - check if it is an alphanumeric charactere.
+* @c: the charactere to be checked.
+* Return: 1 if it's a lowercase - 0 if it's not a lowercase.
+**/
 
 int _isalpha(int c)
 {
-	if ((c <='z' && c >= 'a') || (c <= 'Z' && c >= 'A'))
+	if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A'))
 	{
 	return (1);
 	}
