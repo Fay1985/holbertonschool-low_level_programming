56848f8e916d199f219fe89e2ace1647fce6bf84
Fay1985 <faycal.hadji@live.fr>
2025-02-22 22:35:59 +0000

    update 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- *
- *
- *
- *
+ *_abs - function gives absolute value of an integer
+ *@a: The parameter is an integer
+ *Return: returns the absolute value
  */
 int _abs(int a)
 {
@@ -19,5 +18,4 @@ else
 {
 return (0);
 }
-
 }
