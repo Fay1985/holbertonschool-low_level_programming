ec68965fe97590bd712d9f7db6cb469165926c60
Fay1985 <faycal.hadji@live.fr>
2025-02-22 13:47:08 +0000

    update3

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,26 +1,26 @@
 #include "main.h"
 /**
+ *print_sign - a function that prints signs
+ *@n: it takes an integer
  *
- *
- *
+ *Return: 1 for positive
  */
 
 int print_sign(int n)
 {
-	if( n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-if( n < 0)
-        {
-		_putchar('-');
-                return (-1);
-        }
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
 else
-        {
-		_putchar('0');
-                return (0);
-        }
-
+{
+_putchar('0');
+return (0);
+}
 }
