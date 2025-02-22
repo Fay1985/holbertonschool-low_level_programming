577f518e8482ce4c21077a445cf0718a8311a5fc
Fay1985 <faycal.hadji@live.fr>
2025-02-22 13:15:55 +0000

    update3

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+
+int print_sign(int n)
+{
+	if( n > 0)
+	{
+		return ('+');
+	}
+if( n < 0)
+        {
+                return ('-');
+        }
+else
+        {
+                return ('0');
+        }
+
+}
