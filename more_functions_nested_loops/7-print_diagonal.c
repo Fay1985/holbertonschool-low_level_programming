db92294fccbaeadca5ce9efdcc0782886f4af10d
Fay1985 <faycal.hadji@live.fr>
2025-02-24 14:22:27 +0000

    update7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ *
+ *
+ *
+ *
+ *
+ */
+
+void print_diagonal(int n)
+{
+int j;
+int i;
+
+for (i = 1; i<= n; i++)
+{
+for (j = 1; j <= i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+if (n <=0)
+_putchar('\n');
+}
