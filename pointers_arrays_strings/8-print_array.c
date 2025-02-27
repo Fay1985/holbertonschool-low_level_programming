5e65adbd5654aa78c5a89cb27aea9a38078476bf
Fay1985 <faycal.hadji@live.fr>
2025-02-27 15:08:06 +0000

    helo

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_array - function that prints array.
+ *@a: firt parameter.
+ *@n: second parameter.
+ *Return: prints functions parameters.
+ */
+
 void print_array(int *a, int n)
 {
 int i;
