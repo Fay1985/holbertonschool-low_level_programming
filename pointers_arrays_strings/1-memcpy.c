d8608187fd11e2081adccac54b3fe1886a68d327
Fay1985 <faycal.hadji@live.fr>
2025-03-03 14:09:00 +0000

    task2

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,3 +1,17 @@
+#include "main.h"
+
+/**
+ * _memcpy - a function that copies memory.
+ *
+ *@dest:a parameter to hold destination copy
+ *@src:a memory to hold source of the copy
+ *@n: to hold the number of bytes that are copied
+ *Return: it returns the destination copy that was copied from source.
+ */
+
+
+
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int count;
