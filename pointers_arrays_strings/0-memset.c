2fecc7616831e26d9d6aaa4c8fc5296c652c7556
Fay1985 <faycal.hadji@live.fr>
2025-03-03 12:00:34 +0000

    0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -5,18 +5,16 @@
  *
  *
  */
-
 char *_memset(char *s, char b, unsigned int n)
+{
 {
 unsigned int count = 0;
 while(count < n)
 {
-if (n < 1)
-{
-return 0;
-}
 _putchar((*s + count) * b);
 count++;
 }
-return (0);
+_putchar('\n');
+return (s);
+}
 }
