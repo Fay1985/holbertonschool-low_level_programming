074438eb4e744d6c78aade70d3f8c7a497678c6f
Fay1985 <faycal.hadji@live.fr>
2025-03-03 14:18:52 +0000

    task2

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -14,7 +14,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 unsigned int count;
 for ( count =0; count < n; count++)
 {
-	src[count] = dest[count];
+dest[count] = src[count];
 }
 
 return (dest);
