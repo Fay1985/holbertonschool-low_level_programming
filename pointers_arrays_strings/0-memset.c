bc8bbd56111fe9575fc0847ec0ea12794e3d00b7
Fay1985 <faycal.hadji@live.fr>
2025-03-03 12:16:07 +0000

    file1

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
-/*
- *
- *
- *
+/**
+ * _memset - Fills memory with a constant byte
+ * @s: Pointer to the memory area
+ * @b: The byte to fill memory with
+ * @n: Number of bytes to fill
+ * Return: Pointer to memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-{
 unsigned int count = 0;
-while(count < n)
+
+while (count < n)
 {
-_putchar((*s + count) * b);
+s[count] = b;
 count++;
 }
-_putchar('\n');
-return (s);
-}
+return s;
 }
