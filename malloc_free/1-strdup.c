3875aecb46f2b8336c99439f004079d0bf9e6ae7
Fay1985 <faycal.hadji@live.fr>
2025-03-13 12:20:40 +0000

    hello

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -13,23 +13,20 @@ unsigned int i, j;
 if (str == NULL)
 return NULL;
 
-/* Find the length of the string */
 i = 0;
 while (str[i] != '\0') {
 i++;
 }
 
-/* Allocate memory for the new string */
-dup_str = (char *)malloc((i + 1) * sizeof(char));
+dup_str = (char *)malloc(sizeof(i+1));
+
 if (dup_str == NULL)
 return NULL;
 
-/* Copy the string into the new allocated space */
 for (j = 0; j < i; j++) {
 dup_str[j] = str[j];
 }
 
-/* Null-terminate the new string */
 dup_str[i] = '\0';
 
 return dup_str;
