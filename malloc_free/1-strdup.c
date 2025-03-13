91a3efc896f5d9254370b4067daa835cb8b5df45
Fay1985 <faycal.hadji@live.fr>
2025-03-13 12:27:52 +0000

    hello

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -6,28 +6,33 @@
  *Return: it returns the copied value.
  */
 
-char *_strdup(char *str) {
+char *_strdup(char *str)
+{
 char *dup_str;
 unsigned int i, j;
 
 if (str == NULL)
-return NULL;
-
+{
+return (NULL);
+}
 i = 0;
-while (str[i] != '\0') {
+while (str[i] != '\0')
+{
 i++;
 }
 
-dup_str = (char *)malloc(sizeof(i+1));
+dup_str = malloc(sizeof(i + 1));
 
 if (dup_str == NULL)
-return NULL;
-
-for (j = 0; j < i; j++) {
+{
+return (NULL);
+}
+for (j = 0; j < i; j++)
+{
 dup_str[j] = str[j];
 }
 
 dup_str[i] = '\0';
 
-return dup_str;
+return (dup_str);
 }
